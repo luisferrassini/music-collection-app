9c14ea78185c93a97cf9820b142fc211f1a7a801
unknown <lferrassini@gmail.com>
2021-08-03 14:57:32 -0300

    Correcting a typo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>Music Collection App}</title>
+    <title>Music Collection App</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
